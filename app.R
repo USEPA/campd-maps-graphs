64e8c3afc2e334ce13dc202f17e2a80a86bb04b6
kstetsyuk <stetsyuk.karina@epa.gov>
2022-02-01 12:28:13 -0500

    build scripts for vendor buildpack (windows and mac)

--- app.R
+++ app.R
@@ -1,4 +1,9 @@
+library(shiny)
+library(dotenv)
+
+load_dot_env(".env")
+
 appfold <- "01-app"
 
-#shiny::runApp(appfold)
-shiny::runApp(appfold, host="0.0.0.0", port=strtoi(Sys.getenv("PORT")))
+#runApp(appfold)
+runApp(appfold, host="0.0.0.0", port=strtoi(Sys.getenv("PORT")))
