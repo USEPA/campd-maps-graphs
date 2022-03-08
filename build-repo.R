42e7dd28e024ab565b11a64eb1badd3b6353ab0e
kstetsyuk <stetsyuk.karina@epa.gov>
2022-03-08 14:01:36 -0500

    new build-repo file

--- build-repo.R
+++ build-repo.R
@@ -1,31 +1,17 @@
 # make sure to install.packages("miniCRAN")
 library(miniCRAN)
 
-pkgs <- c("shiny","dotenv","shinydashboard","shinybusy",
-          "leaflet","plotly","tidyverse","lubridate",
-          "dplyr","leaflet.extras","raster", "units")
+pkgs <- c("shiny", "dotenv","shinydashboard","shinybusy",
+          "plotly","tidyverse","lubridate",
+          "dplyr","leaflet.extras")
 # Update r.yml file!
 
 localCRAN <- file.path(getwd(), "vendor_r")
 
-contribDir <- file.path(localCRAN, "src", "contrib")
-
-binPaths <- list(
-  win.binary = file.path("bin/windows/contrib"),
-  mac.binary = file.path("bin/macosx/contrib"),
-  mac.binary.mavericks = file.path("bin/macosx/mavericks/contrib"),
-  mac.binary.leopard = file.path("bin/macosx/leopard/contrib")
-)
-
-binPaths <- lapply(binPaths, function(x) file.path(localCRAN, x))
-
 revolution <- c(CRAN = "https://cran.r-project.org")
 
-pkgList <- pkgDep(pkgs, repos = revolution, type = "source", suggests = FALSE)
+pkgList <- pkgDep(pkgs, repos = revolution, type = "source", suggests = TRUE)
 
-makeRepo(pkgList, path = localCRAN, repos = revolution, 
-         type = c("source", "win.binary", "mac.binary"))
+makeRepo(pkgList, path = localCRAN, repos = revolution)
 
-lapply(binPaths, function(path) {
-  tools::write_PACKAGES(path)
-})
+
