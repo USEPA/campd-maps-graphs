84287ed8a2282657bc646263e7acb0a9c1fe35c8
kstetsyuk <stetsyuk.karina@epa.gov>
2022-03-08 14:07:14 -0500

    New build repo

--- build-repo.R
+++ build-repo.R
@@ -1,10 +1,16 @@
+# Go here to clone repo when using leaflet.
+# https://github.com/testorg-2020/cflinuxfs3-CRAN/tree/master/cflinuxfs3/src/contrib
+
+# Otherwise edit the pkgs to include all packages for the app
+# you are deploying
+
+# Remember to change the r.yml to include all the packages
+
 # make sure to install.packages("miniCRAN")
 library(miniCRAN)
 
 pkgs <- c("shiny", "dotenv","shinydashboard","shinybusy",
-          "plotly","tidyverse","lubridate",
-          "dplyr","leaflet.extras")
-# Update r.yml file!
+          "plotly","tidyverse","lubridate")
 
 localCRAN <- file.path(getwd(), "vendor_r")
 
